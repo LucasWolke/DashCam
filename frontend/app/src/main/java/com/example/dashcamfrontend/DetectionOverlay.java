ae0c827302e8f663dc44b5abf84fbd03b0cd0ca0
Lucas Wolkersdorfer <wolke2807@gmail.com>
2023-04-06 22:34:28 +0200

    #11 Added method that parses the response and creates rectangles array

--- frontend/app/src/main/java/com/example/dashcamfrontend/DetectionOverlay.java
+++ frontend/app/src/main/java/com/example/dashcamfrontend/DetectionOverlay.java
@@ -30,6 +30,49 @@ public class DetectionOverlay extends View {
     }
 
     /**
+     * Saves the cords from the response in the rectangles array and calls setRects.
+     *
+     * Still needs more comments - calculation is not obvious without them!
+     *
+     * @param response the response from the backend, containing the bounding box cords
+     */
+    public void parseResponse(String response) {
+        ObjectMapper mapper = new ObjectMapper();
+        String[] coordinates;
+        try {
+            coordinates = mapper.readValue(response, String[].class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        RectF[] rectangles = new RectF[coordinates.length];
+
+        for (int i = 0; i < coordinates.length; i++) {
+            String [] cords = coordinates[i].split(",");
+            if(cords.length == 4) {
+
+                int screenHeight = this.getHeight();
+                int screenWidth = this.getWidth();
+
+                float realHeight = screenWidth*0.75F;
+                float heightDiff = (realHeight - screenHeight) / 2;
+
+                float left = (Float.parseFloat(cords[0])/640)*screenWidth;
+                float top = (((Float.parseFloat(cords[1]))/480)*realHeight);
+                float right = (Float.parseFloat(cords[2])/640)*screenWidth;
+                float bottom = (((Float.parseFloat(cords[3]))/480)*realHeight);
+
+                bottom -= heightDiff;
+                top -= heightDiff;
+
+                RectF exampleRectangle2 = new RectF(left, top, right, bottom);
+                rectangles[i] = exampleRectangle2;
+            }
+            setRects(rectangles);
+        }
+    }
+
+    /**
      *
      * Sets the private variable rectangles, and invalidates the view
      * so onDraw is called.
