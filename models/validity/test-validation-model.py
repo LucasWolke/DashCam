9e2212f3d5790a0641e88c1dc1c6616542c5ecab
Lucas Wolkersdorfer <wolke2807@gmail.com>
2023-04-21 16:36:59 +0200

    #9 Added python script that tests the model accuracy on training data

---
+++ models/validity/test-validation-model.py
@@ -0,0 +1,36 @@
+import numpy as np
+import os
+import numpy as np
+from PIL import Image, ImageOps
+import tensorflow as tf
+
+import csv
+from csv import reader
+
+model = tf.keras.models.load_model('./simple-validity-model')
+
+# read in correct labels from csv file and store them in an array 
+
+total = 0
+correct = 0
+
+wrong = []
+
+with open('traffic_signs.csv', 'r', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    # iterave over entire training data
+    for row in reader:
+        total += 1
+        test = (int(row[0]),int(row[1]))
+        arr = [tf.expand_dims(test, 0)]
+        predictions = model.predict(arr)
+        # gets actual label from model prediction and compares it to correct label 
+        if(str(np.argmax(tf.nn.softmax(predictions[0]))) == str(row[2])):
+            correct += 1
+        else:
+            # save wrong inputs to learn where shortcomings of model lie
+            wrong.append((int(row[0]),int(row[1])))
+print(total)
+print(correct)
+print(correct/total)
+print(wrong)
